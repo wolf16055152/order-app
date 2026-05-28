ef8a5b31f960e8032d51de9e77c4a5ddfefb37f0
blue-wolf0406 <wolf16055152@gmail.com>
2026-05-29 00:33:33 +0900

    프런트엔드 배포 준비 완료료

--- ui/src/api/client.js
+++ ui/src/api/client.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://localhost:3001/api'
+const API_BASE_URL = (
+  import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001/api'
+).replace(/\/$/, '')
 
 async function request(path, options = {}) {
   const response = await fetch(`${API_BASE_URL}${path}`, {
